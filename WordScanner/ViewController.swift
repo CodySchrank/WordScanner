daefcb5cdbe845f4271d4fa9df2366b97b85ebd5
Cody Schrank <cody.schrank@gmail.com>
2015-05-06 10:18:04 -0700

    Initial Commit

---
+++ WordScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordScanner
+//
+//  Created by Cody Schrank on 5/6/15.
+//  Copyright (c) 2015 TheTapAttack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
